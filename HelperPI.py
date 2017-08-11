c0bdc8b62d6b7496c9e0b7b6bdf0015ac1dc89fe
FloobtheGreat <irwin.phillip@gmail.com>
2017-08-11 14:00:52 -0500

    Version 0.1.0
    Simplified logging

--- HelperPI.py
+++ HelperPI.py
@@ -14,14 +14,8 @@ import pandas as pd
 class Helper:
     def __init__(self):
         self.data = None
-        self.databasecon = r'DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0'
-        ch = logging.StreamHandler()
-        ch.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        ch.setFormatter(formatter)
-        
-        logging.getLogger().addHandler(ch)
-        
+        self.databasecon = r'DRIVER={NetezzaSQL};SERVER=SRVDWHITP01;DATABASE=EDW_SPOKE;UID=pairwin;PWD=pairwin;TIMEOUT=0'        
+        logging.basicConfig(level=logging.INFO)
         logging.info('Initializing...')
         
     def getSQL(self, path):
@@ -30,6 +24,9 @@ class Helper:
             return self.data
         
     def readData(self, sql, temp_dir):
+        '''
+            Returns Dataframe and Data type dataframe
+        '''
         logging.info('Reading data...')
         cnxn = pyodbc.connect(self.databasecon)   
         df = pd.read_sql(sql, cnxn)
