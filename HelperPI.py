e4cd2d262af9caee6e727afcf1643c9d6fb39eb7
FloobtheGreat <irwin.phillip@gmail.com>
2017-09-19 14:02:11 -0500

    Added plot_confusion_matrix

--- HelperPI.py
+++ HelperPI.py
@@ -5,11 +5,15 @@ Created on Fri Aug 11 10:04:09 2017
 @author: pairwin
 Adding a comment for testing branch
 """
-
+import itertools
 import pyodbc
 import logging
 import pandas as pd
 import sklearn.model_selection as cv
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 
 class Helper:
@@ -48,7 +52,41 @@ class Helper:
     def splitData(self, df, trainsize):
         train, test = cv.train_test_split(df, train_size = trainsize)
         return train, test
-        
+    
+    def plot_confusion_matrix(cm, classes,
+                              normalize=False,
+                              title='Confusion matrix',
+                              cmap=plt.cm.Blues):
+        """
+        This function prints and plots the confusion matrix.
+        Normalization can be applied by setting `normalize=True`.
+        """
+        if normalize:
+            cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+            print("Normalized confusion matrix")
+        else:
+            print('Confusion matrix, without normalization')
+    
+        print(cm)
+    
+        plt.imshow(cm, interpolation='nearest', cmap=cmap)
+        plt.title(title)
+        plt.colorbar()
+        tick_marks = np.arange(len(classes))
+        plt.xticks(tick_marks, classes, rotation=45)
+        plt.yticks(tick_marks, classes)
+    
+        fmt = '.2f' if normalize else 'd'
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], fmt),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+    
+        plt.tight_layout()
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        plt.show()
         
    
     
